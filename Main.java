df33dbc4887f88d7da54a68c1e8b24107d5a0e17
jakubik307 <and.jj.an@gmail.com>
2022-12-08 00:23:01 +0100

    Add space-adding function

--- Main.java
+++ Main.java
@@ -1,7 +1,23 @@
 package chucknorris;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void addSpace() {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder input = new StringBuilder();
+
+        input.append(scanner.nextLine());
+
+        for (int i = 0; i < input.length(); i++) {
+            System.out.print(input.charAt(i));
+            if (i != input.length() - 1) System.out.print(" ");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Input string: ");
+        addSpace();
     }
 }
