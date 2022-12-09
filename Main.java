d350a8350d1c9e420a8a1ecf7892019f95f6c6c6
jakubik307 <and.jj.an@gmail.com>
2022-12-09 13:47:19 +0100

    Add unary code

--- Main.java
+++ Main.java
@@ -4,22 +4,56 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void decode() {
+    public static String decode() {
         Scanner scanner = new Scanner(System.in);
         StringBuilder input = new StringBuilder();
+        StringBuilder output = new StringBuilder();
 
         input.append(scanner.nextLine());
 
-        System.out.println("The result:\n");
+        System.out.println("The result:");
 
         for (int i = 0; i < input.length(); i++) {
-            System.out.printf("%c = %s", input.charAt(i), String.format("%7s", Integer.toBinaryString(input.charAt(i))).replace(" ", "0"));
-            if (i != input.length() - 1) System.out.println();
+            output.append(String.format("%7s", Integer.toBinaryString(input.charAt(i))).replace(" ", "0"));
+        }
+        return output.toString();
+    }
+
+    public static void unaryCode(String s) {
+        int digit;
+        int counter = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                counter++;
+            } else {
+                digit = s.charAt(i);
+                if (digit == '0') {
+                    System.out.print("00 ");
+                } else if (digit == '1') {
+                    System.out.print("0 ");
+                }
+
+                for (int j = 0; j < counter + 1; j++) {
+                    System.out.print("0");
+                }
+                System.out.print(" ");
+                counter = 0;
+            }
+        }
+        if (s.charAt(s.length() - 1) == '0') {
+            System.out.print("00 ");
+        } else if (s.charAt(s.length() - 1) == '1') {
+            System.out.print("0 ");
+        }
+        for (int j = 0; j < counter + 1; j++) {
+            System.out.print("0");
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Input string: ");
-        decode();
+
+        unaryCode(decode());
     }
 }
