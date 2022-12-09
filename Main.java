e6758f5cf88d2dd1f1a9e977668f772c23993f48
jakubik307 <and.jj.an@gmail.com>
2022-12-09 01:04:20 +0100

    Add binary translation

--- Main.java
+++ Main.java
@@ -4,20 +4,22 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void addSpace() {
+    public static void decode() {
         Scanner scanner = new Scanner(System.in);
         StringBuilder input = new StringBuilder();
 
         input.append(scanner.nextLine());
 
+        System.out.println("The result:\n");
+
         for (int i = 0; i < input.length(); i++) {
-            System.out.print(input.charAt(i));
-            if (i != input.length() - 1) System.out.print(" ");
+            System.out.printf("%c = %s", input.charAt(i), String.format("%7s", Integer.toBinaryString(input.charAt(i))).replace(" ", "0"));
+            if (i != input.length() - 1) System.out.println();
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Input string: ");
-        addSpace();
+        decode();
     }
 }
